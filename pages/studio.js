a1c50c772878b058013ca398dab1059ddd8ce6d8
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-06-01 12:52:03 -0700

    studio page

--- pages/studio.js
+++ pages/studio.js
@@ -19,15 +19,47 @@ const studio = () => {
             <Image
               src="/../public/MainStudio.jpg"
               alt="Picture of Dryw's studio"
-              width={500}
-              height={400}
+              width={600}
+              height={500}
             />
           </Carousel.Item>
           <Carousel.Item>
             <Image
-              src="/../public/DrywStudio.jpg"
-              alt="Picture of Dryw in his Studio"
-              width={500}
+              src="/../public/AmberLiveRoom2.jpg"
+              alt="Picture of Amber De La Rosa in the Live Room"
+              width={550}
+              height={500}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              src="/../public/DrywRedShoes.jpg"
+              alt="Picture of Dryw from the back in studio"
+              width={550}
+              height={500}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              src="/../public/DrywWithBassist.jpg"
+              alt="Picture of Dryw from the back in studio with Bassist"
+              width={550}
+              height={500}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              src="/../public/DrywWithGuitarPlayer.jpg"
+              alt="Picture of Dryw from the back in studio with Guitarist"
+              width={550}
+              height={500}
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <Image
+              src="/../public/LiveRoomDoor.jpg"
+              alt="Picture of Live Room Door"
+              width={550}
               height={500}
             />
           </Carousel.Item>
